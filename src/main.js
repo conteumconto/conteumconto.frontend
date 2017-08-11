33c3806115ec16dd930ec46494f82e2d35c18bc5
Lucas Ricardo <llucasrgt@gmail.com>
2017-08-11 20:54:52 -0300

    update authorizarion and token verification

--- src/main.js
+++ src/main.js
@@ -10,13 +10,15 @@ import 'vue-material/dist/vue-material.css'
 import VueSweetAlert from 'vue-sweetalert'
 
 import Home from './components/home/main'
-import Painel from './components/home/painel'
+import TeacherHome from './components/home/main.teacher'
 import Signup from './components/signup/root.signup'
 import BookCase from './components/bookcase/main.vue'
 import EditProfile from './components/editProfile/editProfile'
 import Login from './components/login/login'
 import Write from './components/write/write.vue'
 import Auth from './auth/index'
+
+import common from './services/CommonServices'
 
 Vue.use(VueMaterial)
 Vue.use(VueResource)
@@ -25,18 +27,17 @@ Vue.use(Vuex)
 Vue.use(VueSweetAlert)
 
 Vue.http.headers.common['Authorization'] = localStorage.getItem('token')
-Auth.checkAuth()
 
 const routes = [
-  {path: '/home', alias: '/', component: Home, meta: {requiresAuth: true}},
-  {path: '/painel', alias: '/painelDoProfessor', component: Painel, meta: {requiresAuth: true}},
-  {path: '/books', alias: '/meusLivros', component: BookCase, meta: {requiresAuth: true}},
-  { path: '/editProfile', alias: '/editarPerfil', component: EditProfile, meta: {requiresAuth: true} },
+  { path: '/home', alias: '/', component: Home, meta: {requiresStudentAuth: true} },
+  { path: '/painel', alias: '/painelDoProfessor', component: TeacherHome, meta: {requiresTeacherAuth: true} },
+  { path: '/books', alias: '/meusLivros', component: BookCase, meta: {requiresStudentAuth: true} },
+  { path: '/editProfile', alias: '/editarPerfil', component: EditProfile, meta: {requiresStudentAuth: true} },
   { path: '/signup', alias: '/cadastrar', component: Signup },
   { path: '/login', alias: '/entrar', name: 'login', component: Login },
-  { path: '/write', alias: '/escrever', component: Write, meta: {requiresAuth: true} },
-  { path: '/write/new/:book_id/', alias: '/escrever/novo/:book_id/', component: Write, meta: {requiresAuth: true} },
-  { path: '/write/new/:chapter_id/', alias: '/escrever/editar/:chapter_id/', component: Write, meta: {requiresAuth: true} }
+  { path: '/write', alias: '/escrever', component: Write, meta: {requiresStudentAuth: true} },
+  { path: '/write/new/:book_id/', alias: '/escrever/novo/:book_id/', component: Write, meta: {requiresStudentAuth: true} },
+  { path: '/write/new/:chapter_id/', alias: '/escrever/editar/:chapter_id/', component: Write, meta: {requiresStudentAuth: true} }
 ]
 // Create the router instance and pass the `routes` option
 const router = new VueRouter({
@@ -44,12 +45,16 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  if (to.meta.requiresAuth) {
-    if (localStorage.getItem('token') == null || localStorage.getItem('student') === 'undefined') {
+  // Verify if it's a protected route
+  if (to.meta.requiresStudentAuth || to.meta.requiresTeacherAuth) {
+    // Checks if a token and the actor of the protected route is present in the localStorage
+    if (!common.isEmpty(localStorage.getItem('token')) &&
+       (to.meta.requiresStudentAuth && !common.isEmpty(localStorage.getItem('Student'))) ||
+       (to.meta.requiresTeacherAuth && !common.isEmpty(localStorage.getItem('Teacher')))) {
+      next()
+    } else {
       next({name: 'login'})
       Auth.logout()
-    } else {
-      next()
     }
   }
   next()
