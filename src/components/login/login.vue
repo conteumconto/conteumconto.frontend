a4de1f66e9f313dee0b5ef61889c2c7e261c676d
luandryl <luan.andryl@gmail.com>
2017-09-24 04:45:34 -0300

    rewrite page html

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,31 +1,3 @@
-<template>
-  <div id="login">
-    <loginTopnav :page="'login'"></loginTopnav>
-    <div class="container">
-      <form @submit="onSubmit" onsubmit="return false">
-        <div class="form-group">
-          <div style="margin-top: 4em" class="form-text"> LOGIN </div>
-          <input required type="login" v-model="credentials.login" v-bind:class="{danger:isDanger}" name="login" placeholder="Digite seu login">
-        </div>
-        <div class="form-group">
-          <div class="form-text"> SENHA </div>
-          <div class="password-input-wrapper">
-            <input required type="password" v-if="hidePassword" v-model="credentials.password" minlength="5" v-bind:class="{danger:isDanger}" name="senha" placeholder="Digite sua senha">
-            <input required type="text" v-else v-model="credentials.password" minlength="5" v-bind:class="{danger:isDanger}" name="senha" placeholder="Digite sua senha">
-            <i class="fa fa-eye toggle-password" aria-hidden="true" v-if="hidePassword" @click="hidePassword = !hidePassword"></i>
-            <i class="fa fa-eye-slash toggle-password" aria-hidden="true" v-else @click="hidePassword = !hidePassword"></i>
-          </div>
-        </div>
-        <button type="submit" class="btn btn-red"> ENTRAR </button>
-        <md-snackbar :md-position="snackBar.vertical + ' ' + snackBar.horizontal" ref="snackbar" :md-duration="snackBar.duration">
-          <span>{{situationText}}</span>
-          <md-button class="md-accent" md-theme="light-blue" @click.native="$refs.snackbar.close()">OK</md-button>
-        </md-snackbar>
-      </form>
-    </div>
-  </div>
-</template>
-
 <script>
   import loginTopnav from '../common/loginTopnav'
   import auth from '../../auth'
@@ -67,18 +39,94 @@
     }
   }
 </script>
+<template>
+  <div class="login-wrapper">
+    <div class="logo">
+      <a href="#"><img src="../../assets/img/logo.png" height='64' width='261' alt="logo" /></a>
+      <h1> Escrever Aprender Ensinar</h1>
+    </div>
 
+    <div class="login">
+      <h1 class="title-welcome">Login </h1>
+      <div class="container">
+        <form @submit="onSubmit" onsubmit="return false">
+          <div class="form-group">
+            <div style="margin-top: 4em" class="form-text"> Usuário </div>
+            <input required type="login" v-model="credentials.login" v-bind:class="{danger:isDanger}" name="login" placeholder="Digite seu usuário">
+          </div>
+          <div class="form-group">
+            <div class="form-text"> SENHA </div>
+            <div class="password-input-wrapper">
+              <input required type="password" v-if="hidePassword" v-model="credentials.password" minlength="5" v-bind:class="{danger:isDanger}" name="senha" placeholder="Digite sua senha">
+              <input required type="text" v-else v-model="credentials.password" minlength="5" v-bind:class="{danger:isDanger}" name="senha" placeholder="Digite sua senha">
+              <i class="fa fa-eye toggle-password" aria-hidden="true" v-if="hidePassword" @click="hidePassword = !hidePassword"></i>
+              <i class="fa fa-eye-slash toggle-password" aria-hidden="true" v-else @click="hidePassword = !hidePassword"></i>
+            </div>
+          </div>
+          <button type="submit" class="btn btn-green"> ENTRAR </button>
+          <md-snackbar :md-position="snackBar.vertical + ' ' + snackBar.horizontal" ref="snackbar" :md-duration="snackBar.duration">
+            <span>{{situationText}}</span>
+            <md-button class="md-accent" md-theme="light-blue" @click.native="$refs.snackbar.close()">OK</md-button>
+          </md-snackbar>
+        </form>
+        
+      </div>
+      <div class="login-footer">
+        <a class="form-text" href="#/cadastrar">cadastre-se</a> <br />
+        <a class="form-text" href="#/">Esqueceu sua senha?</a>
+      </div>
+    </div>
+    
+  </div>
+  
+</template>
 <style scoped lang="sass">
   @import './../../assets/bases.sass'
 
   $gray-input: #bbd;
   $formWidth: 420px;
 
-  #login
-    height: 100%;
-    background-color: $blue-lightier;
-    position: relative;
+  .login-wrapper
+    width: 100%
+    height: 100%
+    background: green
+    display: flex
+    flex-flow: row wrap
+
+  .logo, .login
+    flex: 1 1
   
+  .logo
+    background: blue
+    background: url('./../../assets/img/login_bg.jpg') no-repeat center center fixed
+    box-shadow: inset 0 0 0 1000px rgba(12, 83, 157, 0.5);
+    -webkit-background-size: cover
+    -moz-background-size: cover
+    background-size: cover
+    -o-background-size: cover
+    display: flex
+    justify-content: center
+    align-items: center
+    flex-flow: column wrap
+
+  .logo h1
+    font-family: $title_font
+    color: #fff
+    font-size: 1.2rem
+  
+  .login
+    background: #fff
+    display: flex
+    justify-content: center
+    align-items: center
+    display: flex
+    justify-content: center
+    align-items: center
+    flex-flow: column wrap
+
+  .login-footer
+    text-align: center
+
   .container
     margin: 5em auto;
     width: 35em;
@@ -97,7 +145,7 @@
     text-align: left;
     font-family: $title_alter_font!important;
     font-weight: 500;
-    color: $blue_dark
+    color: $blue_dark!important
 
   input
     margin: auto;
@@ -122,5 +170,5 @@
     color: #668;
 
   .danger
-    border: 1px solid red 
+    border: 1px solid red
 </style>
