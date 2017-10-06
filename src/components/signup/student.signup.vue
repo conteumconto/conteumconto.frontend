d3755a55f6f597cbdac926b08ea2bba4ed669916
luandryl <luan.andryl@gmail.com>
2017-10-06 15:15:01 -0300

    component transition

--- src/components/signup/student.signup.vue
+++ src/components/signup/student.signup.vue
@@ -1,45 +1,3 @@
-<template>
-  <div id="student-signup">
-    <div class="container">
-      <form @submit="onSubmit" onsubmit="return false">
-        <div class="form-group">
-          <div class="form-text">NOME</div>
-          <input required type="text" v-model="student.first_name" minlength="3" maxlength="100" name="nome" placeholder="Digite seu nome">
-        </div>
-        <div class="form-group">
-          <div class="form-text">SOBRENOME</div>
-          <input required type="text" v-model="student.last_name" minlength="3" name="sobreNome" placeholder="Digite seu sobrenome">
-        </div>
-        <div class="form-group">
-          <div class="form-text">IDADE</div>
-          <input required type="number" v-model="student.birth_day" name="idade" min="1" max="100" placeholder="Qual a sua idade?">
-        </div>
-        <div class="form-group">
-          <div class="form-text">EMAIL</div>
-          <input required type="email" v-model="student.email" name="email" placeholder="Digite seu email">
-        </div>
-        <div class="form-group">
-          <div class="form-text">LOGIN</div>
-          <input required type="login" v-model="student.login" name="login" placeholder="Crie um login para a sua conta">
-        </div>
-        <div class="form-group">
-          <div class="form-text">SENHA</div>
-          <div class="password-input-wrapper">
-            <input required type="password" v-if="hidePassword" v-model="student.password" minlength="5" name="senha" placeholder="Crie uma senha">
-            <input required type="text" v-else v-model="student.password" minlength="5" name="senha" placeholder="Crie uma senha">
-            <i class="fa fa-eye toggle-password" aria-hidden="true" v-if="hidePassword" @click="hidePassword = !hidePassword"></i>
-            <i class="fa fa-eye-slash toggle-password" aria-hidden="true" v-else @click="hidePassword = !hidePassword"></i>
-          </div>
-        </div>
-        <button type="submit" class="btn btn-green"> CADASTRAR </button>
-        <md-snackbar :md-position="snackBar.vertical + ' ' + snackBar.horizontal" ref="snackbar" :md-duration="snackBar.duration">
-          <span>{{situationText}}</span>
-        </md-snackbar>
-      </form>
-    </div>
-  </div>
-</template>
-
 <script>
   import loginTopnav from '../common/loginTopnav'
   import auth from '../../auth'
@@ -79,6 +37,53 @@
     }
   }
 </script>
+<template>
+<transition
+  name="bounce"
+  enter-active-class="bounceInRight"
+  leave-active-class="bounceOutRight"
+>
+  <div id="student-signup">
+    <div class="container">
+      <form @submit="onSubmit" onsubmit="return false">
+        <div class="form-group">
+          <div class="form-text">NOME</div>
+          <input required type="text" v-model="student.first_name" minlength="3" maxlength="100" name="nome" placeholder="Digite seu nome">
+        </div>
+        <div class="form-group">
+          <div class="form-text">SOBRENOME</div>
+          <input required type="text" v-model="student.last_name" minlength="3" name="sobreNome" placeholder="Digite seu sobrenome">
+        </div>
+        <div class="form-group">
+          <div class="form-text">IDADE</div>
+          <input required type="number" v-model="student.birth_day" name="idade" min="1" max="100" placeholder="Qual a sua idade?">
+        </div>
+        <div class="form-group">
+          <div class="form-text">EMAIL</div>
+          <input required type="email" v-model="student.email" name="email" placeholder="Digite seu email">
+        </div>
+        <div class="form-group">
+          <div class="form-text">LOGIN</div>
+          <input required type="login" v-model="student.login" name="login" placeholder="Crie um login para a sua conta">
+        </div>
+        <div class="form-group">
+          <div class="form-text">SENHA</div>
+          <div class="password-input-wrapper">
+            <input required type="password" v-if="hidePassword" v-model="student.password" minlength="5" name="senha" placeholder="Crie uma senha">
+            <input required type="text" v-else v-model="student.password" minlength="5" name="senha" placeholder="Crie uma senha">
+            <i class="fa fa-eye toggle-password" aria-hidden="true" v-if="hidePassword" @click="hidePassword = !hidePassword"></i>
+            <i class="fa fa-eye-slash toggle-password" aria-hidden="true" v-else @click="hidePassword = !hidePassword"></i>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-green"> CADASTRAR </button>
+        <md-snackbar :md-position="snackBar.vertical + ' ' + snackBar.horizontal" ref="snackbar" :md-duration="snackBar.duration">
+          <span>{{situationText}}</span>
+        </md-snackbar>
+      </form>
+    </div>
+  </div>
+</transition>
+</template>
 
 <style scoped lang="sass">
   @import './../../assets/bases.sass'
@@ -94,7 +99,6 @@
     background-color: white;
   
   form
-    height: 100%;
     margin: auto;  
     text-align: center;
   
