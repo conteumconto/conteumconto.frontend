010725ee00d0044c1dbedda7a1507e9f8ef06144
luandryl <luan.andryl@gmail.com>
2017-10-07 11:52:49 -0300

    hashbang remove

--- src/main.js
+++ src/main.js
@@ -43,7 +43,8 @@ const routes = [
 ]
 // Create the router instance and pass the `routes` option
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
 router.beforeEach((to, from, next) => {
