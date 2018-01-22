17ea28ef81514f61ba5e19a6e6422dacea7ab163
Davi Kawasaki <davishinjik@gmail.com>
2018-01-22 00:42:29 -0200

    Basic refactor + api with own http service treating promises with catching errors

--- src/components/home/main.vue
+++ src/components/home/main.vue
@@ -1,50 +1,50 @@
-<script>
-import topnav from '../common/topnav.vue'
-import ListBook from '../common/book/list_book.vue'
+<template>
+  <div>
+    <topnav v-bind:toggleNav='true' ></topnav>
+    <div class="welcome">
+      <h1 class="title-welcome">Bem Vindo ao Projeto Conte um Conto!</h1>
+      <div class="text">
+        <p class="content">Olá amiguinho, nós queremos te ajudar a escrever melhor. Esta é a página inicial e aqui fica o livro que você está escrevendo junto com seu professor.</p>
+      </div>
+    </div>
+    <ListBook v-bind:home='true'></ListBook>
+  </div>
+</template>
 
-export default {
-  name: 'home',
-  components: {
-    topnav,
-    ListBook
-  },
-  computed: {
-    student_data: function () {
-      return this.$store.getters.dataStudent
+<script>
+  import topnav from '../common/main.topnav'
+  import ListBook from '../common/book/list.book'
+
+  export default {
+    name: 'home',
+    components: {
+      topnav,
+      ListBook
+    },
+    computed: {
+      student_data () {
+        return this.$store.getters.dataStudent
+      }
     }
   }
-}
 </script>
-<template>
 
-<div>
-  <topnav v-bind:toggleNav='true' ></topnav>
-  <div class="welcome">
-    <h1 class="title-welcome">Bem Vindo ao Projeto Conte um Conto!</h1>
-    <div class="text">
-      <p class="content">Olá amiguinho, nós queremos te ajudar a escrever melhor. Esta é a página inicial e aqui fica o livro que você está escrevendo junto com seu professor.</p>
-    </div>
-  </div>
-  <ListBook v-bind:home='true'></ListBook>
-</div>
-
-</template>
 <style scoped lang="sass">
-@import './../../assets/bases.sass'
-.ct-btn
-  display: flex
-  justify-content: center
-.welcome
-  background: #fff
-  padding: .5rem
-  text-align: center
-  display: flex
-  flex-direction: column
-  align-items: center
+  @import './../../assets/bases.sass'
 
-.text
-  width: 800px
-  margin-bottom: 1rem
+  .ct-btn
+    display: flex
+    justify-content: center
 
+  .welcome
+    background: #fff
+    padding: .5rem
+    text-align: center
+    display: flex
+    flex-direction: column
+    align-items: center
 
+  .text
+    width: 800px
+    margin-bottom: 1rem
 </style>
--- src/components/home/painel.vue
+++ src/components/home/painel.vue
@@ -1,40 +1,40 @@
-<script>
-import topnav from '../common/topnav.vue'
-export default {
-  name: 'home',
-  components: {
-    topnav
-  }
-}
-</script>
 <template>
-
-<div>
-  <div class="welcome">
-    <h1 class="title-welcome">Olá Professor, bem Vindo ao Projeto Conte um Conto!</h1>
-    <div class="text">
-      <p class="content">Olá amigão, nós queremos te ajudar a escrever melhor. Esta é a página inicial e aqui fica os livros dos seus alunos! :D.</p>
+  <div>
+    <div class="welcome">
+      <h1 class="title-welcome">Olá Professor, seja bem vindo ao Projeto Conte um Conto!</h1>
+      <div class="text">
+        <p class="content">Olá amigão, nós queremos te ajudar a escrever melhor. Esta é a página inicial e aqui fica os livros dos seus alunos! :D.</p>
+      </div>
     </div>
   </div>
-</div>
-
 </template>
+
+<script>
+  import topnav from '../common/main.topnav'
+  export default {
+    name: 'home',
+    components: {
+      topnav
+    }
+  }
+</script>
+
 <style scoped lang="sass">
-@import './../../assets/bases.sass'
+  @import './../../assets/bases.sass'
 
-.ct-btn
-  display: flex
-  justify-content: center
-.welcome
-  background: #fff
-  padding: .5rem
-  text-align: center
-  display: flex
-  flex-direction: column
-  align-items: center
+  .ct-btn
+    display: flex
+    justify-content: center
+    
+  .welcome
+    background: #fff
+    padding: .5rem
+    text-align: center
+    display: flex
+    flex-direction: column
+    align-items: center
 
-.text
-  width: 800px
-  margin-bottom: 1rem
-
+  .text
+    width: 800px
+    margin-bottom: 1rem
 </style>
