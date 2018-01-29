61611e689cfa4bf59958f74279bf5f7fdf7e83eb
Davi Kawasaki <davishinjik@gmail.com>
2018-01-29 18:46:20 -0200

    BookStore: refactors + actualBookID + book/chapter memory CRUD actions

--- src/store/modules/book.store.js
+++ src/store/modules/book.store.js
@@ -1,4 +1,4 @@
-import InjectChapters from '../../services/Chapter.service'
+import ChapterService from '../../services/Chapter.service'
 import Auth from '../../services/auth.service'
 import CucHttpService from '../../services/Cuc.Http.service'
 import common from '../../services/common.service'
@@ -8,7 +8,8 @@ let cucHttp = new CucHttpService('conteumconto', Auth.getAuthHeader())
 // state
 const state = {
   books: [],
-  haveBooks: true
+  haveBooks: true,
+  actualBook: null
 }
 
 // getters
@@ -18,6 +19,9 @@ const getters = {
   },
   haveBooks (state) {
     return state.haveBooks
+  },
+  actualBook () {
+    return state.actualBook
   }
 }
 
@@ -35,7 +39,7 @@ const actions = {
         if (response.length === 0) {
           commit('NO_BOOKS')
         } else {
-          commit('LOAD_BOOKS', InjectChapters.resolve(response))
+          commit('LOAD_BOOKS', ChapterService.resolve(response))
           commit('HAVE_BOOKS')
         }
       })
@@ -52,11 +56,93 @@ const actions = {
     let student = JSON.parse(localStorage.getItem('Student'))
     if (!common.isEmpty(student)) {
       if (common.isEmptyArray(student.books)) commit('NO_BOOKS')
-      else commit('HAVE_BOOKS')
+      else {
+        commit('LOAD_BOOKS', student.books)
+        commit('HAVE_BOOKS')
+      }
     } else {
       Auth.logout()
       this.$router.push({name: 'login'})
     }
+  },
+
+  storeNewBookData ({commit}, newBook) {
+    let student = JSON.parse(localStorage.getItem('Student'))
+    if (!common.isEmpty(student)) {
+      if (common.isEmpty(student.books)) student.books = []
+      student.books.push(newBook)
+      localStorage.setItem('Student', JSON.stringify(student))
+      commit('LOAD_BOOKS', student.books)
+      commit('HAVE_BOOKS')
+    } else {
+      Auth.logout()
+      this.$router.push({name: 'login'})
+    }
+  },
+
+  storeNewChapterData ({commit}, newChapter) {
+    let student = JSON.parse(localStorage.getItem('Student'))
+    if (!common.isEmpty(student)) {
+      if (common.isEmpty(student.books)) throw new Error('student_books_not_found')
+      else {
+        student.books = ChapterService.addChapter(student.books, newChapter)
+        localStorage.setItem('Student', JSON.stringify(student))
+        commit('LOAD_BOOKS', student.books)
+        commit('HAVE_BOOKS')
+      }
+    } else {
+      Auth.logout()
+      this.$router.push({name: 'login'})
+    }
+  },
+
+  updateChapterData ({commit}, updatedChapter) {
+    let student = JSON.parse(localStorage.getItem('Student'))
+    if (!common.isEmpty(student)) {
+      if (common.isEmpty(student.books)) throw new Error('student_books_not_found')
+      else {
+        student.books = ChapterService.updateChapter(student.books, updatedChapter)
+        localStorage.setItem('Student', JSON.stringify(student))
+        commit('LOAD_BOOKS', student.books)
+        commit('HAVE_BOOKS')
+      }
+    } else {
+      Auth.logout()
+      this.$router.push({name: 'login'})
+    }
+  },
+
+  updateBookData ({commit}, updatedBook) {
+    let student = JSON.parse(localStorage.getItem('Student'))
+    if (!common.isEmpty(student)) {
+      if (common.isEmpty(student.books)) throw new Error('student_books_not_found')
+      else {
+        if (updatedBook.deleted) student.books = student.books.filter(book => book._id !== updatedBook._id)
+        else {
+          student.books = student.books.map(book => {
+            if (book._id === updatedBook._id) {
+              const chapters = JSON.parse(JSON.stringify(book.chapters))
+              book = updatedBook
+              book.chapters = chapters
+            }
+            return book
+          })
+        }
+        localStorage.setItem('Student', JSON.stringify(student))
+        if (common.isEmptyArray(student.books)) commit('NO_BOOKS')
+        else {
+          commit('LOAD_BOOKS', student.books)
+          commit('HAVE_BOOKS')
+        }
+      }
+    } else {
+      Auth.logout()
+      this.$router.push({name: 'login'})
+    }
+  },
+
+  setActualBook ({commit}, bookId) {
+    commit('ACTUAL_BOOK', bookId)
   }
 }
 
@@ -70,6 +156,9 @@ const mutations = {
   },
   HAVE_BOOKS (STATE) {
     state.haveBooks = true
+  },
+  ACTUAL_BOOK (state, bookId) {
+    state.actualBook = bookId
   }
 }
 
