9295ed243c89894d4feb1e554deb3f5663f36da5
Davi Kawasaki <davishinjik@gmail.com>
2018-01-29 18:40:15 -0200

    Welcome text change for main page

--- src/components/home/main.vue
+++ src/components/home/main.vue
@@ -2,9 +2,9 @@
   <div>
     <topnav v-bind:toggleNav='true' ></topnav>
     <div class="welcome">
-      <h1 class="title-welcome">Bem Vindo ao Projeto Conte um Conto!</h1>
+      <h1 class="title-welcome">Bem vindo ao Projeto Conte um Conto!</h1>
       <div class="text">
-        <p class="content">Olá amiguinho, nós queremos te ajudar a escrever melhor. Esta é a página inicial e aqui fica o livro que você está escrevendo junto com seu professor.</p>
+        <p class="content">Olá amiguinho, nós queremos te ajudar a escrever melhor. Esta é a página inicial e aqui ficam os livros que você está escrevendo junto com seu professor.</p>
       </div>
     </div>
     <ListBook v-bind:home='true'></ListBook>
